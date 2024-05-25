001ed1773d46b2d1a891b527d63c071c278b38e4
gutierrez <pablogutierrezreis@gmail.com>
2024-05-25 16:49:14 -0300

    Projeto Finalizado

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+ScrollReveal().reveal('#carouselExampleInterval',{
+    origin: 'left',
+    duration:2000,
+    distance:'20%'
+});
+
+ScrollReveal().reveal('#img-historia',{
+    origin: 'left',
+    duration:3000,
+    distance:'20%'
+});
+ScrollReveal().reveal('.title-2',{
+    origin: 'right',
+    duration:1000,
+    distance:'20%'
+});
+ScrollReveal().reveal('.text-2',{
+    origin: 'right',
+    duration:1000,
+    distance:'20%'
+});
+
+ScrollReveal().reveal('.qualidades',{
+    origin: 'left',
+    duration:2000,
+    distance:'20%'
+});
+ScrollReveal().reveal('.text-qualidades',{
+    origin: 'left',
+    duration:1000,
+    distance:'20%'
+});
+
+ScrollReveal().reveal('#testimonials_content',{
+    origin: 'right',
+    duration:1000,
+    distance:'20%'
+});
+
+ScrollReveal().reveal('#testimonial_chef',{
+    origin: 'left',
+    duration:3000,
+    distance:'20%'
+});
